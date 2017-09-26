5d69bb163a79c8b6a12328f7b6550a2483c2645c
Cakarena <30563088+Cakarena@users.noreply.github.com>
2017-09-26 10:54:23 +1000

    Add files via upload

---
+++ Exercise-7.2.3.py
@@ -0,0 +1,19 @@
+fname = input('Enter the file name: ')
+
+if fname == 'na na boo boo':
+	print ("NA NA BOO BOO, You have been punk'd! ")
+try: 
+	fhand = open(fname)
+except:
+	print ('File cannot be opened:', fname)
+	exit()
+
+fhand = open(fname)
+count = 0
+for line in fhand:
+	if line.startswith('Subject:'):
+		count = count + 1
+print('There were', count, 'subject lines in', fname)
+
+
+
